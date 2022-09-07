8a0e48eaab2ec4aed8c909889bf25752e2bfdaa3
radhi-17 <106006117+radhi-17@users.noreply.github.com>
2022-09-07 20:44:31 +0530

    Update ec2instance.tf

--- ec2instance.tf
+++ ec2instance.tf
@@ -1,6 +1,6 @@
 provider "aws" {
 region = "us-east-1"
-version = "v2.70.0"
+#version = "v2.70.0"
 }
 resource "aws_instance" "myec2" {
 ami = "ami-0022f774911c1d690"
@@ -12,5 +12,5 @@ tags = {
 }
 }  
 output "publicip" {
-  value = "aws_instance.myec2.public_ip"
+  value = aws_instance.myec2.public_ip
 }
